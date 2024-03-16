84b93346357a2e650c543de6d96e3a3bb4774e69
Alieza Nuriman <115769623+Aliezan@users.noreply.github.com>
2024-03-16 16:48:24 +0700

    fix: navbar cond. rendering

--- src/components/navbar/Navibar.tsx
+++ src/components/navbar/Navibar.tsx
@@ -11,13 +11,16 @@ import {
   NavbarMenuItem,
 } from "@nextui-org/react";
 import { SpaceGrotesk } from "@/utils/font";
-import { Link } from "react-scroll/modules";
+import { Link as LinkScroll } from "react-scroll/modules";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 import Logo from "../Logo";
 import ThemeButton from "./ThemeButton";
 import { Button } from "../ui/button";
 
 const Navibar: FC = () => {
   const [isMenuOpen, setIsMenuOpen] = useState<boolean>(false);
+  const path = usePathname();
 
   return (
     <Navbar
@@ -35,92 +38,87 @@ const Navibar: FC = () => {
           <Logo width={100} height={100} />
         </NavbarBrand>
       </NavbarContent>
-      <NavbarContent className="hidden gap-4 sm:flex" justify="center">
-        <NavbarItem>
-          <Button variant="link">
-            <Link
-              to="hero"
-              className={SpaceGrotesk.className}
-              spy
-              smooth
-              offset={-70}
-              duration={500}
-            >
-              home
-            </Link>
-          </Button>
-        </NavbarItem>
-        <NavbarItem>
-          <Button variant="link">
-            <Link
-              to="about"
-              className={SpaceGrotesk.className}
-              spy
-              smooth
-              offset={60}
-              duration={500}
-            >
-              about
-            </Link>
-          </Button>
-        </NavbarItem>
-        <NavbarItem>
-          <Button variant="link">
-            <Link
-              to="works"
-              className={SpaceGrotesk.className}
-              spy
-              smooth
-              offset={-30}
-              duration={500}
-            >
-              works
-            </Link>
-          </Button>
-        </NavbarItem>
-        <NavbarItem>
-          <Button variant="link">
-            <Link
-              to="works"
-              className={SpaceGrotesk.className}
-              spy
-              smooth
-              offset={-30}
-              duration={500}
-            >
-              blogs
-            </Link>
-          </Button>
-        </NavbarItem>
-      </NavbarContent>
+      {path === "/" && (
+        <>
+          <NavbarContent className="hidden gap-4 sm:flex" justify="center">
+            <NavbarItem>
+              <Button variant="link">
+                <LinkScroll
+                  to="hero"
+                  className={SpaceGrotesk.className}
+                  spy
+                  smooth
+                  offset={-70}
+                  duration={500}
+                >
+                  home
+                </LinkScroll>
+              </Button>
+            </NavbarItem>
+            <NavbarItem>
+              <Button variant="link">
+                <LinkScroll
+                  to="about"
+                  className={SpaceGrotesk.className}
+                  spy
+                  smooth
+                  offset={60}
+                  duration={500}
+                >
+                  about
+                </LinkScroll>
+              </Button>
+            </NavbarItem>
+            <NavbarItem>
+              <Button variant="link">
+                <LinkScroll
+                  to="works"
+                  className={SpaceGrotesk.className}
+                  spy
+                  smooth
+                  offset={-30}
+                  duration={500}
+                >
+                  works
+                </LinkScroll>
+              </Button>
+            </NavbarItem>
+            <NavbarItem>
+              <Button variant="link">
+                <Link href="/blogs">blogs</Link>
+              </Button>
+            </NavbarItem>
+          </NavbarContent>
+          <NavbarMenu>
+            <NavbarMenuItem>
+              <Button variant="link">
+                <LinkScroll to="home" spy smooth duration={500}>
+                  home
+                </LinkScroll>
+              </Button>
+            </NavbarMenuItem>
+            <NavbarMenuItem>
+              <Button variant="link">
+                <LinkScroll to="about" spy smooth duration={500}>
+                  about
+                </LinkScroll>
+              </Button>
+            </NavbarMenuItem>
+            <NavbarMenuItem>
+              <Button variant="link">
+                <LinkScroll to="test" spy smooth duration={500}>
+                  works
+                </LinkScroll>
+              </Button>
+            </NavbarMenuItem>
+          </NavbarMenu>
+        </>
+      )}
       <NavbarContent justify="end">
         <NavbarItem>
           <ThemeButton />
         </NavbarItem>
       </NavbarContent>
-      <NavbarMenu>
-        <NavbarMenuItem>
-          <Button variant="link">
-            <Link to="home" spy smooth duration={500}>
-              home
-            </Link>
-          </Button>
-        </NavbarMenuItem>
-        <NavbarMenuItem>
-          <Button variant="link">
-            <Link to="about" spy smooth duration={500}>
-              about
-            </Link>
-          </Button>
-        </NavbarMenuItem>
-        <NavbarMenuItem>
-          <Button variant="link">
-            <Link to="test" spy smooth duration={500}>
-              works
-            </Link>
-          </Button>
-        </NavbarMenuItem>
-      </NavbarMenu>
     </Navbar>
   );
 };
